3b76ee274db81c43d8e3eb83ae510106fe5aa4bb
chrischang2 <chris.c8912@gmail.com>
2026-01-13 14:35:57 +1300

    left some comments on what dave said about this part whren i ran into him this morning

---
+++ 11_Regression_of_invoice_totals_per_period.py
@@ -0,0 +1,6 @@
+# i talked to dave about this
+# he recommends trying a handful of different approaches, e.g. simple OLS regression, maybe based on 2025 data only because 
+# the shape of the invoice totals cruve changes after start of 2025.
+# dont let it stop you from using all data also
+# can also try a simple time series regression, maybe ChatGPT can give us some suggestions on methods.
+# we can look at trying to model month-by-month, or cumulatively. just try whatever you think might works
