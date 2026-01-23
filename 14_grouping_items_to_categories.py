372b3ebfe06d8018d082ea0d6e2067fa2e745e30
chrischang2 <chris.c8912@gmail.com>
2026-01-24 10:44:23 +1300

    made script for zhaoyu as example

---
+++ 14_grouping_items_to_categories.py
@@ -0,0 +1,15 @@
+'''
+Docstring for 14_grouping_items_to_categories
+
+this script is for zhaoyu to parse the description columns in 13.1_invoice_line_items_still_unmatched.py
+you will have to make a guess about what the discount is
+
+
+'''
+
+nexgard spectra - vet mediaction 
+
+writing code to get average discount amount for vets in merchant discount detail File:
+vet1 = 5
+vet2 = 10
+average vet = 8%
